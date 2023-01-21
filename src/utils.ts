623020a180e3f71a47a163e4d719ad15c8e49a32
Elpis-alpha <elpis409@gmail.com>
2023-01-21 20:56:34 +0100

    init

---
+++ src/utils.ts
@@ -0,0 +1,223 @@
+import { v4 } from "uuid";
+
+export const sitesList = [
+  {
+    id: v4(),
+    url: "elpis.cc",
+    name: "My Portfolio",
+    description: `
+      Welcome to my personal portfolio website! Here, you'll find a collection of my past and current projects,
+      as well as information about my background and skills. The website is designed to give you an idea of my 
+      experience and capabilities as a professional. The site is easy to navigate, with clear sections for my 
+      projects, skills, services, and contact information. You can also find links to my social media profiles and other
+      online presences. I update my portfolio regularly, so be sure to check back often to see my latest work.
+    `,
+    category: [
+      "frontend",
+      "personal",
+      "standalone"
+    ],
+    alternate: [
+
+    ]
+  },
+  {
+    id: v4(),
+    url: "sites.elpis.cc",
+    name: "Domain Sites",
+    description: "This is the website you are currently looking at. It displays a list of all the websites I have on this domain.",
+    category: [
+      "frontend",
+      "personal",
+      "standalone"
+    ],
+    alternate: []
+  },
+  {
+    id: v4(),
+    url: "adaptable-stores.elpis.cc",
+    name: "Adaptable Stores",
+    description: "A dummy e-commerce store which I made in order to become experienced and acquainted with popular payment gateway services such as stripe and paypal.",
+    category: [
+      "frontend",
+      "ecommerce",
+    ],
+    alternate: [
+      "as.elpis.cc"
+    ]
+  },
+  {
+    id: v4(),
+    url: "aide-memoire.elpis.cc",
+    name: "Aide-memoire",
+    description: "Welcome to my Aide-memoire! In this website, users can save their thoughts, ideas, and musings for easy accessibility. The site is easy to navigate, with clear sections for notes, tags and a search bar to find a specific note. You can also make notes public for others to access.",
+    category: [
+      "frontend",
+    ],
+    alternate: [
+      "am.elpis.cc"
+    ]
+  },
+  {
+    id: v4(),
+    url: "little-gathering.elpis.cc",
+    name: "Little Gathering",
+    description: "Welcome to my Little Gathering! Here, users can connect with like-minded individuals and discuss a wide range of topics. The website is designed to be a place where users can share their ideas and experiences, and get feedback from others. The site is easy to navigate, and it utilizes the thread layout. Users can create their own threads and comment on existing ones.",
+    category: [
+      "frontend",
+      "social"
+    ],
+    alternate: [
+      "lg.elpis.cc"
+    ]
+  },
+  {
+    id: v4(),
+    url: "weather.elpis.cc",
+    name: "Weather",
+    description: "Welcome to my weather website! Here, you'll find up-to-date information on current weather conditions and forecasts for locations around the world. The website is designed to provide users with accurate and reliable weather information, including current temperature, humidity, wind speed, and more. The site is easy to navigate, with a search bar to quickly find the weather for a specific location.",
+    category: [
+      "frontend",
+    ],
+    alternate: []
+  },
+  {
+    id: v4(),
+    url: "ai.elpis.cc",
+    name: "My A.I",
+    description: "Welcome to my simple AI website! While I gave it a fancy name, it's really just a simplified organizer which may help in self-monitoring one's activities.",
+    category: [
+      "frontend",
+      "standalone",
+    ],
+    alternate: []
+  },
+  {
+    id: v4(),
+    url: "api.aide-memoire.elpis.cc",
+    name: "Aide-memoire API",
+    description: "This is the backend api for Aide-memoire",
+    category: [
+      "backend",
+    ],
+    alternate: []
+  },
+  {
+    id: v4(),
+    url: "api.as.elpis.cc",
+    name: "Adaptable Stores API",
+    description: "This is the backend api for Adaptable Stores",
+    category: [
+      "backend",
+      "ecommerce",
+    ],
+    alternate: []
+  },
+  {
+    id: v4(),
+    url: "api.elpis.cc",
+    name: "My Backend",
+    description: "This is my personal backend. I use it to collect data from various websites that I design as well as perform certain operations to keep some websites functional.",
+    category: [
+      "backend",
+      "personal",
+    ],
+    alternate: []
+  },
+  {
+    id: v4(),
+    url: "api.lg.elpis.cc",
+    name: "Little Gathering API",
+    description: "This is the backend api for Little Gathering.",
+    category: [
+      "backend",
+      "social",
+    ],
+    alternate: []
+  },
+  {
+    id: v4(),
+    url: "api.weather.elpis.cc",
+    name: "Weather API",
+    description: "This is the backend api for my weather website.",
+    category: [
+      "backend",
+    ],
+    alternate: []
+  },
+  {
+    id: v4(),
+    url: "landing-pages.elpis.cc",
+    name: "Landing Pages",
+    description: "Welcome to my landing pages website directory! Here, you'll find a list of some of the landing pages websites I designed to improve myself. The website is designed to provide visitors with a very simplified list of the landing pages that I designed at the time.",
+    category: [
+      "frontend",
+      "standalone",
+      "personal",
+    ],
+    alternate: []
+  },
+  {
+    id: v4(),
+    url: "little-works.elpis.cc",
+    name: "Little Works",
+    description: "Welcome to my little works directory! Here, you'll find a list of the little works I did when I started learning web development. It's not much but it's worth it.",
+    category: [
+      "frontend",
+      "standalone",
+      "personal",
+      "game"
+    ],
+    alternate: []
+  },
+  {
+    id: v4(),
+    url: "ludo.elpis.cc",
+    name: "Ludo",
+    description: "Welcome to my Ludo game website! Here, you can play one of the most popular board games of all time, Ludo, locally with friends or against my highly trained AI. The website is designed to provide users with a fun and engaging experience, with beautiful graphics and smooth gameplay. The site is easy to navigate, with a simple interface to play the game, and also has the option to play with different number of players, 2, 3 or 4 players.",
+    category: [
+      "frontend",
+      "standalone",
+      "game"
+    ],
+    alternate: []
+  },
+  {
+    id: v4(),
+    url: "old-portfolio.elpis.cc",
+    name: "My Old Portfolio",
+    description: "This is just my old portfolio, nothing special.",
+    category: [
+      "standalone",
+      "frontend",
+      "personal",
+    ],
+    alternate: []
+  },
+  {
+    id: v4(),
+    url: "tic-tac-toe.elpis.cc",
+    name: "Tic Tac Toe",
+    description: "Welcome to my Tic-Tac-Toe game website! Here, you can play one of the most classic and timeless games of all time, Tic-Tac-Toe, locally with friends or against my highly trained AI. The website is designed to provide users with a simple and fun experience, with easy-to-use interface and different levels of difficulty. The site is easy to navigate, with a simple interface to play the game, and edit it's settings.",
+    category: [
+      "frontend",
+      "standalone",
+      "game"
+    ],
+    alternate: [
+
+    ]
+  },
+  {
+    id: v4(),
+    url: "words.elpis.cc",
+    name: "",
+    description: "A little interface I created because I had difficulty naming a website I wanted to make. The name of the website is Nyux Whispers. If it is not on this list, then it is still in development. Look foward to it.",
+    category: [
+      "frontend",
+      "standalone",
+      "personal"
+    ],
+    alternate: []
+  },
+]
